2a4d271e56e5a58539a89082501c0f73a973488f
tahmidul612 <tahmidul612@gmail.com>
2024-05-24 01:12:02 -0400

    feat: create json object from html table
    remove_escape_chars processes the content of the generated json to remove escape characters

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
+import html_to_json
+import re
+def remove_escape_chars(table_json):
+    for row in table_json:
+        for key in row:
+            row[key] = re.sub('\s+', ' ', bytes(str(row[key]), 'utf-8').decode('utf-8'))
+    return table_json
 def main():
     url = "https://www.rcmusic.com/ggs/master-classes-and-performances/student-recitals-and-community-performances"
 
@@ -12,5 +19,11 @@ def main():
     for row in soup.find_all("tr"):
         table_html += str(row)
     table_html += "\n</table>"
+
+    # Convert the table_html to json
+    # table_json is a list of dictionaries (each list is a row in the table)
+    # Each dictionary has three keys: "Date & Time", "Location", and "Artist & Discipline"
+
+    table_json = remove_escape_chars(html_to_json.convert_tables(table_html)[0])
 if __name__ == '__main__':
     main()
