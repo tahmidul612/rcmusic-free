652797bcc67c00de713f1b0d95f21bcd4020e6f3
tahmidul612 <tahmidul612@gmail.com>
2024-05-24 01:26:24 -0400

    style: fix formatting

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import html_to_json
 import re
-from icalendar import Calendar, Event, vCalAddress, vText
+from icalendar import Calendar, Event, vText
 from datetime import datetime, timedelta
 from pathlib import Path
 import os
@@ -12,8 +12,10 @@ import pytz
 def remove_escape_chars(table_json):
     for row in table_json:
         for key in row:
-            row[key] = re.sub('\s+', ' ', bytes(str(row[key]), 'utf-8').decode('utf-8'))
+            row[key] = re.sub(
+                '\s+', ' ', bytes(str(row[key]), 'utf-8').decode('utf-8'))
     return table_json
+
 
 def create_ics(table_json):
     cal = Calendar()
@@ -23,7 +25,8 @@ def create_ics(table_json):
         event = Event()
         event.add('summary', entry['Artist & Discipline'])
         event['location'] = vText(entry['Location'])
-        start_time = datetime.strptime(entry['Date & Time'], '%A, %B %d %I:%M%p').replace(tzinfo=pytz.timezone('America/Toronto')).replace(year=datetime.now().year)
+        start_time = datetime.strptime(entry['Date & Time'], '%A, %B %d %I:%M%p').replace(
+            tzinfo=pytz.timezone('America/Toronto')).replace(year=datetime.now().year)
         event.add('dtstart', start_time)
         event.add('dtend', start_time + timedelta(hours=1))
         cal.add_component(event)
@@ -40,6 +43,7 @@ def create_ics(table_json):
     f = open(os.path.join(directory, 'all_concerts.ics'), 'wb')
     f.write(cal.to_ical())
     f.close()
+
 
 def main():
     url = "https://www.rcmusic.com/ggs/master-classes-and-performances/student-recitals-and-community-performances"
@@ -58,10 +62,12 @@ def main():
     # table_json is a list of dictionaries (each list is a row in the table)
     # Each dictionary has three keys: "Date & Time", "Location", and "Artist & Discipline"
 
-    table_json = remove_escape_chars(html_to_json.convert_tables(table_html)[0])
-    
+    table_json = remove_escape_chars(
+        html_to_json.convert_tables(table_html)[0])
+
     # Create the ics file
     create_ics(table_json)
+
 
 if __name__ == '__main__':
     main()
